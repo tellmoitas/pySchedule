b0fc36aa7528e1b8986d0895e64f60490b10106a
tellmoitas_labs <tell.moitas@academico.ifrn.edu.br>
2020-05-11 17:30:02 -0300

    update pySchedule.py

--- pySchedule.py
+++ pySchedule.py
@@ -1,1 +1,34 @@
 # Como Agendar Scripts Python & Execução Paralela
+
+import schedule
+import time
+
+def tarefa():
+    print("Executando tarefa")
+
+def job():
+    print("I'm working...")
+
+# definindo intervalo
+schedule.every(10).seconds.do(tarefa)
+schedule.every(10).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every(1).week.do(tarefa)
+schedule.every(3).day.do(tarefa)
+schedule.every(1).day.at('08:00').do(tarefa)
+schedule.every(2).hour.do(tarefa)
+
+
+schedule.every().day.at("10:30").do(job)
+schedule.every(5).to(10).minutes.do(job) #random (5, 10)
+schedule.every().monday.do(job)
+schedule.every().wednesday.at("13:15").do(job)
+schedule.every().minute.at(":17").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
+
+
+
